8d06c972acbf291273ce5b1b1a9e4f67e3a7fe55
shakya445 <shakyaluja445@gmail.com>
2020-12-26 23:18:02 +0545

    updates the expired API urls with new one

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,60 @@ import 'package:emergencyBhaktapur/Process/APIRequest.dart';
 import 'package:flutter/material.dart';
 import 'Views/Home.dart';
 import 'package:emergencyBhaktapur/Views/Home.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  APIRequest apiRequest1 = new APIRequest(st: 'ambulance',apiUrl: 'http://neofusionsoc.com/dataEmergency/ambulance');
-  APIRequest apiRequest2 = new APIRequest(st: 'hospital',apiUrl: 'http://neofusionsoc.com/dataEmergency/hospital');
-  APIRequest apiRequest3 = new APIRequest(st: 'fireBrigade',apiUrl: 'http://neofusionsoc.com/dataEmergency/fireBrigade');
-  APIRequest apiRequest4 = new APIRequest(st: 'police',apiUrl: 'http://neofusionsoc.com/dataEmergency/police');
-  APIRequest apiRequest5 = new APIRequest(st: 'sabbahan',apiUrl: 'http://neofusionsoc.com/dataEmergency/sabbahan');
-  APIRequest apiRequest6 = new APIRequest(st: 'bhaktapur',apiUrl: 'http://neofusionsoc.com/dataEmergency/bhaktapur');
-  APIRequest apiRequest7 = new APIRequest(st: 'changu',apiUrl: 'http://neofusionsoc.com/dataEmergency/changu');
-  APIRequest apiRequest8 = new APIRequest(st: 'thimi',apiUrl: 'http://neofusionsoc.com/dataEmergency/thimi');
-  APIRequest apiRequest9 = new APIRequest(st: 'suryabinayak',apiUrl: 'http://neofusionsoc.com/dataEmergency/suryabinayak');
-  APIRequest apiRequest10= new APIRequest(st: 'covid',apiUrl: 'http://neofusionsoc.com/dataEmergency/covid');
-  APIRequest apiRequest11= new APIRequest(st: 'office',apiUrl: 'http://neofusionsoc.com/dataEmergency/office');
-  APIRequest apiRequest12= new APIRequest(st: 'blood',apiUrl: 'http://neofusionsoc.com/dataEmergency/blood');
+  APIRequest apiRequest1 = new APIRequest(
+      st: 'ambulance',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/ambulance.php');
+  APIRequest apiRequest2 = new APIRequest(
+      st: 'hospital',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/hospital.php');
+  APIRequest apiRequest3 = new APIRequest(
+      st: 'fireBrigade',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/fireBrigade.php');
+  APIRequest apiRequest4 = new APIRequest(
+      st: 'police',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/police.php');
+  APIRequest apiRequest5 = new APIRequest(
+      st: 'sabbahan',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/sabbahan.php');
+  APIRequest apiRequest6 = new APIRequest(
+      st: 'bhaktapur',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/bhaktapur.php');
+  APIRequest apiRequest7 = new APIRequest(
+      st: 'changu',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/changu.php');
+  APIRequest apiRequest8 = new APIRequest(
+      st: 'thimi',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/thimi.php');
+  APIRequest apiRequest9 = new APIRequest(
+      st: 'suryabinayak',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/suryabinayak.php');
+  APIRequest apiRequest10 = new APIRequest(
+      st: 'covid',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/covid.php');
+  APIRequest apiRequest11 = new APIRequest(
+      st: 'office',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/office.php');
+  APIRequest apiRequest12 = new APIRequest(
+      st: 'blood',
+      apiUrl:
+          'https://raw.githubusercontent.com/Techsamuha/Emergency-bhaktapur/master/blood.php');
   @override
   Widget build(BuildContext context) {
     apiRequest1.getData();
@@ -38,7 +76,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      darkTheme:ThemeData(
+      darkTheme: ThemeData(
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
