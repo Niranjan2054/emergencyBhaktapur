851a840ef21bca755c2a57a1c04328e20293440b
banmala <sunilbanmala0@gmail.com>
2021-05-21 09:12:50 +0545

    created folder for data of kathmandu valley

---
+++ data/lalitpur/covid.php
@@ -0,0 +1,114 @@
+[
+	{
+		"id": "1",
+		"title": "Covid Ambulance",
+		"title_nep": "कोभिड एम्बुलेन्स",
+		"contact": "9851320321",
+		"contact_nep": "९८५१३२०३२१",
+		"isTitle":"0"
+	},
+	{
+		"id": "2",
+		"title": "MOHP Hot Line (6am-10pm)",
+		"title_nep": "एम.ओ.एच.पि. हत लाईन(विहान ६-बेलुका १०)",
+		"contact": "1115",
+		"contact_nep": "१११५",
+		"isTitle":"0"
+	},
+	{
+		"id": "2",
+		"title": "MOHP Hot Line (24hrs)",
+		"title_nep": "एम.ओ.एच.पि. हत लाईन(२४ घण्टा)",
+		"contact": "1133",
+		"contact_nep": "११३३",
+		"isTitle":"0"
+	},
+	{
+		"id": "3",
+		"title": "NPHL Teku",
+		"title_nep": "एन पि एच एल,तेकु",
+		"contact": "01-4252421",
+		"contact_nep": "०१-४२५२४२१",
+		"isTitle":"0"
+	},
+	{
+		"id": "4",
+		"title": "MOHP (8am - 8pm)",
+		"title_nep": "एम.ओ.एच.पि. (विहान ८-बेलुका ८)",
+		"contact": "9851255834",
+		"contact_nep": "९८५१२५५८३४",
+		"isTitle":"0"
+	},
+	{
+		"id": "5",
+		"title": "MOHP (8am - 8pm)",
+		"title_nep": "एम.ओ.एच.पि. (विहान ८-बेलुका ८)",
+		"contact": "9851255837",
+		"contact_nep": "९८५१२५५८३७",
+		"isTitle":"0"
+	},
+	{
+		"id": "6",
+		"title": "MOHP (8am - 8pm)",
+		"title_nep": "एम.ओ.एच.पि. (विहान ८-बेलुका ८)",
+		"contact": "9851255839",
+		"contact_nep": "९८५१२५५८३९",
+		"isTitle":"0"
+	},
+	{
+		"id": "7",
+		"title": "Oxygen",
+		"title_nep": "अक्सिजन ",
+		"contact": "",
+		"contact_nep": "",
+		"isTitle":"1"
+	},
+	{
+		"id": "7",
+		"title": "Oxygen 24 hrs.",
+		"title_nep": "अक्सिजन ",
+		"contact": "9851210581",
+		"contact_nep": "९८५१३१०५८१",
+		"isTitle":"0"
+	},
+	{
+		"id": "7",
+		"title": "Oxygen 24 hrs.",
+		"title_nep": "अक्सिजन ",
+		"contact": "9851210584",
+		"contact_nep": "९८५१३१०५८४",
+		"isTitle":"0"
+	},
+	{
+		"id": "7",
+		"title": "Bhaktapur District",
+		"title_nep": "भक्तपुर जिल्ला",
+		"contact": "",
+		"contact_nep": "",
+		"isTitle":"1"
+	},
+	{
+		"id": "7",
+		"title": "Bhaktapur Hospital",
+		"title_nep": "भक्तपुर हस्पितल ",
+		"contact": "01-6610798",
+		"contact_nep": "०१-६६१०७९८",
+		"isTitle":"0"
+	},
+	{
+		"id": "8",
+		"title": "Nepal Korea Friendship Hospital",
+		"title_nep": "नेपाल कोरिया मैत्री हस्पितल",
+		"contact": "01-6633442",
+		"contact_nep": "०१-६६३३४४२",
+		"isTitle":"0"
+	},
+	{
+		"id": "9",
+		"title": "Janaswasthya Karyalaye, Katunje",
+		"title_nep": "जनस्वास्थ्य कार्यालय, कतुन्जे",
+		"contact": "01-6615271",
+		"contact_nep": "०१-६६१५२७१",
+		"isTitle":"0"
+	}	
+]
